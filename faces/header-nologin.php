66a6da36715dacd5a8ece29e41082cc0e3d478dd
JanzChou <39876032+JanzChou@users.noreply.github.com>
2018-06-03 20:08:06 +0700

    Delete header-nologin.php

--- faces/header-nologin.php
+++
@@ -1,9 +0,0 @@
-	<div id="menu">
-    	<ul>
-            <li><a<?php if($menu=='Pemeriksaan Lab'){echo' class="active"';} ?> href="lab.php">Pemeriksaan Lab</a></li>
-            <li><a<?php if($menu=='Jadwal Dokter'){echo' class="active"';} ?> href="jadwal.php">Jadwal Dokter</a></li>
-        </ul>
-    </div>
-	<div id="header">
-    	<img src="assets/img/logo1.png" title="<?= TITLE_WEB; ?>" />
-    </div>
