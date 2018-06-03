774cc1b4b49482e661b56d857c5750ce8c44bada
JanzChou <39876032+JanzChou@users.noreply.github.com>
2018-06-03 20:08:28 +0700

    Add files via upload

---
+++ faces/header-nologin.php
@@ -0,0 +1,10 @@
+	<div id="menu">
+    	<ul>
+            <li><a<?php if($menu=='home'){echo' class="active"';} ?> href="login.php">login</a></li>
+            <li><a<?php if($menu=='Pemeriksaan Lab'){echo' class="active"';} ?> href="lab.php">Pemeriksaan Lab</a></li>
+            <li><a<?php if($menu=='Jadwal Dokter'){echo' class="active"';} ?> href="jadwal.php">Jadwal Dokter</a></li>
+        </ul>
+    </div>
+	<div id="header">
+    	<img src="assets/img/logo1.png" title="<?= TITLE_WEB; ?>" />
+    </div>
